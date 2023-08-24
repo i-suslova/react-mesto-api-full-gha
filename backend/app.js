6924d245dddcd70a48265f97bf8332d2aa362f2d
Irina <i.sus2011@yandex.ru>
2023-08-24 12:27:29 +0300

    logger.js

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const routes = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -18,8 +19,12 @@ app.use(express.json());
 // для обработки данных, отправленных через формы HTML
 app.use(express.urlencoded({ extended: true }));
 
+// подключаем логгер запросов
+app.use(requestLogger);
 // подключаем роуты
 app.use(routes);
+// подключаем логгер ошибок
+app.use(errorLogger);
 // обработчики ошибок 'celebrate'
 app.use(errors());
 // централизованная обработка ошибок

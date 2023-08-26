d0eba12067c5d8e8fb70aec150eb28c70b02145d
Irina <i.sus2011@yandex.ru>
2023-08-26 19:27:28 +0300

    start2

--- backend/utils/constants.js
+++
@@ -1,2 +0,0 @@
-const urlRegex = /^(https?:\/\/)?(www\.)?([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+(\/[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]*)?#?/;
-module.exports = { urlRegex };
--- backend/utils/errors/rateLimitError.js
+++
@@ -1,8 +0,0 @@
-class RateLimitError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 429; // Код состояния 429 - Слишком много запросов
-  }
-}
-
-module.exports = RateLimitError;

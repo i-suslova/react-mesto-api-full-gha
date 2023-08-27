ab807d0af86e0e413ce0e106a67639db1942ba0f
Irina <i.sus2011@yandex.ru>
2023-08-27 18:27:22 +0300

    общий .gitignore

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const routes = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
 mongoose
   .connect('mongodb://localhost:27017/mestodb', {
+    // .connect('mongodb://127.0.0.1:27017/mestodb', {
     useNewUrlParser: true,
   });
 
@@ -18,8 +21,12 @@ app.use(express.json());
 // для обработки данных, отправленных через формы HTML
 app.use(express.urlencoded({ extended: true }));
 
+// подключаем логгер запросов
+app.use(requestLogger);
 // подключаем роуты
 app.use(routes);
+// подключаем логгер ошибок
+app.use(errorLogger);
 // обработчики ошибок 'celebrate'
 app.use(errors());
 // централизованная обработка ошибок
